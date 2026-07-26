817f10e2ea7a06ee4f552890e1da3a9a4cd00628
stickdog24 <mcvilaire@yahoo.com>
2026-07-25 23:39:42 -0400

    started laying out assets for red room

---
+++ Assets/geyser_sideways.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class geysersideways : MonoBehaviour
+{
+    public bool geyserOn;
+    public float currentTime;
+    public GameObject GeyserObject;
+    public GameObject imagesaver;
+    public bool offset;
+
+    public float On;
+    public float Off;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (offset)
+        {
+            GeyserObject.SetActive(true);
+            imagesaver.SetActive(true);
+        geyserOn = true;
+        }
+        else if (!offset)
+        {
+            GeyserObject.SetActive(false);
+            imagesaver.SetActive(false);
+        geyserOn = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime > Off && !geyserOn)
+        {
+            GeyserObject.SetActive(true);
+            imagesaver.SetActive(true);
+            geyserOn = true;
+            currentTime = 0f;
+        }
+        else if(currentTime > On && geyserOn)
+        {
+           GeyserObject.SetActive(false);
+           imagesaver.SetActive(false);
+            geyserOn = false;
+            currentTime = 0f; 
+        }
+    }
+}
