1da4658290c0991986f8479e79f0b9101997993c
perplexingbestiary-cyber <perplexingbestiary@gmail.com>
2026-07-26 00:49:12 -0400

    new scripts for keycards

---
+++ Assets/Scripts/KeyCardScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class KeyCardScript : MonoBehaviour
+{
+    public bool green;
+    public bool blue;
+    public bool red;
+
+    public ElevatorController elevatorController;
+    public GameObject Sprite;
+    public GameObject Gate;
+
+    void OnTriggerEnter2d()
+    {
+        Sprite.SetActive(false);
+        Gate.SetActive(false);
+
+        if (red)
+        {
+            elevatorController.RedKeyCard();
+        }
+        else if (green)
+        {
+            elevatorController.GreenKeyCard();
+        }
+        else if (blue)
+        {
+            elevatorController.EndKeyCard();
+        }
+        
+        
+    }
+}
